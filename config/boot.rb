4041ffeb0ae1e7b51789a7f9c330e1f05c8bc8a3
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-03-01 11:11:15 +0000

    Attempting to deploy to heroku

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+#Removed because it gives errors and i don't understand it
+#require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
