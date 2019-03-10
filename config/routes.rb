22a40278dcf3fd5d5d553e2b89ff090e78c08ad5
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-03-10 20:40:11 +0000

    Basic layout set out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'progress/index'
+  get 'workouts/index'
+  get 'logs/index'
+  get 'exercises/index'
+  get 'exercises', to: redirect('/exercises/index')
+  get 'workouts', to: redirect('/workouts/index')
+  get 'progress', to: redirect('/progress/index')
+  get 'logs', to: redirect('/logs/index')
   get "/", to: "homepage#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/decorators/log_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe LogDecorator do
+end
