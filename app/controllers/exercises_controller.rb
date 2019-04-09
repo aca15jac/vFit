7d3841654cd30c2327dc63597cb9c4653fcaf291
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-04-09 16:21:47 +0100

    Started work on tables in haml

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,8 @@ class ExercisesController < ApplicationController
   before_action :authenticate_user!
   def index
     @title = "Excercises"
-
+    @exercises = Exercise.all
+    @muscles = Muscle.all
   end
 
 end
