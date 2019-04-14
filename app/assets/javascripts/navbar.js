a7365508a50eed8548ac4691776fe0a97e995bd6
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-04-14 19:16:59 +0100

    fin

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("burgermenu").style.color ="#343a40";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("burgermenu").style.color ="#919191";
 }
