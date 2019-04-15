02048852077f3ea327c257b15b2b9bc855711006
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-04-15 13:50:12 +0100

    serviceworker

--- config/initializers/serviceworker.rb
+++ config/initializers/serviceworker.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.serviceworker.routes.draw do
     # map to assets implicitly
     match "/serviceworker.js"
-    match "/manifest.json"
+    match "/manifest.manifest"
 
     # Examples
     #
