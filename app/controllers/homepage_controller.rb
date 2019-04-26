d2bcdab5ae5e9c8d75b4a2c7b5b6983b97020eb4
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-04-26 14:44:44 +0100

    validate homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -10,13 +10,12 @@ class HomepageController < ApplicationController
     if !params[:weightreps].nil?
       @weight = params[:weightreps]["weight"]
       @reps = params[:weightreps]["reps"]
-      if is_number?(@weight) && is_number?(@reps)
+      if is_number?(@weight) && is_number?(@reps) && @reps.to_i < 13
         @weight = @weight.to_f
         @reps = @reps.to_i
       else
         @false_values = true
       end
-
     else
       @false_values = true
     end
