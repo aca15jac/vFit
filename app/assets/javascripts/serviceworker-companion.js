e908f673d7d35d93a38b19694d497e87f4001aab
James Cotcher <jacotcher1@sheffield.ac.uk>
2019-05-01 09:31:47 +0100

    dlkgj

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './users/sign_in' })
+  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
     .then(function(reg) {
       console.log('[Companion]', 'Service worker registered!');
     });
